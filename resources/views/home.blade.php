cb09c22cdf361af04884b3de3340b76da482e604
alhankeser <alhankeser@gmail.com>
2018-09-03 17:17:33 -0400

    Block search engines from crawling dev environment

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta name="robots" content="{{ app()->env == 'production' ? 'index, follow' : 'noindex, nofollow' }}">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1>Home</h1>
+    </body>
+</html>
