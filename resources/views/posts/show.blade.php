dd2cf1c2e54ac81318211bacb0446b0562ad077c
alhankeser <alhankeser@gmail.com>
2018-09-10 00:37:21 -0400

    Make post styling and start basic layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,17 +5,25 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/app.css" class="rel">
         <title>Blog</title>
     </head>
     <body>
-        <a href="/">Home</a>
-        @if (Auth::check())
-        <a href="/posts/{{ $post->id }}/edit">Edit</a>
-            @if (! $post->published)
-            <p>DRAFT</p>
-            @endif
-        @endif
-        <h1>{{ $post->title }}</h1>
-        <p>{!! $post->body !!}</p>
+        <div class="page-container">
+            <div class="post-container slide-up-container time-2">
+                @if (Auth::check())
+                <a href="/posts/{{ $post->id }}/edit">Edit</a>
+                    @if (! $post->published)
+                    <p style="background:yellow; padding: 10px;">DRAFT</p>
+                    @endif
+                @endif
+                <h1 class="fade-in time-2">{{ $post->title }}</h1>
+                <small class="fade-in time-3">{{ $post->date }}</small>
+                <p class="fade-in time-3">{!! $post->body !!}</p>
+            </div>
+            <div class="header fade-in time-4">
+                <div class="logo"><a href="/">alhan.co</a></div>
+            </div>
+        </div>
     </body>
 </html>
