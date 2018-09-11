b76bef39181f7e36f40fff3079bde17b716d3628
alhankeser <alhankeser@gmail.com>
2018-09-11 14:03:46 -0400

    Privacy policy

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,24 +1,24 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+        <title>Blog</title>
         <meta name="robots" content="{{ app()->env == 'production' ? 'index, follow' : 'noindex, nofollow' }}">
         <meta name="description" content="">
-        <link rel="shortcut icon" href="data:image/x-icon;" type="image/x-icon"> 
+        <link rel="shortcut icon" href="data:image/x-icon;" type="image/x-icon">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Blog</title>
         <link rel="preload" as="font" crossorigin="crossorigin" type="font/woff2" href="https://fonts.gstatic.com/s/lora/v12/0QIgMX1D_JOuO7HeNtxumtus-7w.woff2">
         <style>
-                @if (!isset($css)) 
-                    @php
-                        $css = Cache::remember('css', 22*60, function() {
-                            return Storage::disk('public')->get('/css/app.css');
-                        }); 
-                    @endphp
-                @endif
-                {!! $css !!}
-        </style>
+            @if (!isset($css)) 
+                @php
+                    $css = Cache::remember('css', 22*60, function() {
+                        return Storage::disk('public')->get('/css/app.css');
+                    }); 
+                @endphp 
+            @endif
+            {!! $css !!}
+        </style> 
     </head>
     <body>
         <div class="page">
@@ -31,6 +31,9 @@
             </div>
             @endif
             @yield('content')
+            <div class="container footer">
+                    <a href="/privacy">Privacy</a>
+            </div>
         </div>
     </body>
 </html>
--- resources/views/static/privacy.blade.php
+++ resources/views/static/privacy.blade.php
@@ -4,7 +4,17 @@
     <div class="container content-container post-container">
         <h1>Privacy Policy</h1>
         <small></small>
-        <p></p>
+        <p>It's very simple: this website uses <a href="http://analytics.google.com/">Google Analytics</a>, which uses cookies to track <strong>anonymous</strong> user activity, such as page visits and clicks. This information is securely transmitted to Google Analytics and is primarily used to boost the website owner's ego whenever possible.</p>
+        <p>Unless you choose to fill out a web form on the website with your personal information, <strong>this website tracks no personally identifiable information</strong>.</p>
+        <p>Also:
+            <ul>
+                <li>Your anonymous Google Analytics data is not shared with any 3rd parties.</li>
+                <li>Your IP is not stored.</li>
+                <li>The 'user Id' feature is not used in Google Analytics.</li>
+            </ul>
+        </p>
+        <h2>What are cookies?</h2>
+        <p>Cookies are pieces of data stored in your browser and help tools like Google Analytics recognize you as the same visitor when you come back to a website or when you navigate between pages using the same browser.</p>
     </div>
 @endsection
             
