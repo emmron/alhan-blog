ef9765676a8856338cd8f19ac34a26bed0889a62
alhankeser <alhankeser@gmail.com>
2018-09-12 00:04:12 -0400

    Add Meta tag controls

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,30 +1,28 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <title>Blog</title>
+        <title>@yield('title')</title>
+        <meta name="description" content="@yield('description')">
         <meta name="robots" content="{{ app()->env == 'production' ? 'index, follow' : 'noindex, nofollow' }}">
-        <meta name="description" content="">
         <link rel="shortcut icon" href="data:image/x-icon;" type="image/x-icon">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="preload" as="font" crossorigin="crossorigin" type="font/woff2" href="https://fonts.gstatic.com/s/lora/v12/0QIgMX1D_JOuO7HeNtxumtus-7w.woff2">
-        <style>
-            {!! $css !!}
-        </style> 
+        <style>{!! $css !!}</style> 
     </head>
     <body>
-        <div class="page">
-            <div class="container header">
-                <div class="logo"><a href="/">alhan.co</a></div>
+        <div class="container page">
+            <div class="header">
+                <div class="logo"><a href="/">Home</a></div>
             </div>
             @if (Auth::check())
-            <div class="container admin-tools mb-2">
+            <div class="admin-tools mb-2">
                 @yield('admin-tools')
             </div>
             @endif
             @yield('content')
-            <div class="container footer">
+            <div class="footer">
                     <a href="/privacy">Privacy</a>
             </div>
         </div>
